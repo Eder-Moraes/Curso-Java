a9b1e434e3ac44713fb0092743b42fc9654a2223
Eder-Moraes <moraeseder880@gmail.com>
2024-12-05 12:09:49 -0300

    Primeira aula de heranca e polimorfismo

---
+++ Herança e Polimorfismo/HerencaPolimorfismo/src/aula130_heranca/application/Program.java
@@ -0,0 +1,15 @@
+package aula130_heranca.application;
+
+import aula130_heranca.entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+
+	}
+
+}
---
+++ Herança e Polimorfismo/HerencaPolimorfismo/src/aula130_heranca/entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package aula130_heranca.entities;
+
+public class BusinessAccount extends Account{
+	
+	private Double loadLimit;
+	
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loadLimit) {
+		super(number, holder, balance);
+		this.loadLimit = loadLimit;
+	}
+
+	public Double getLoadLimit() {
+		return loadLimit;
+	}
+
+	public void setLoadLimit(Double loadLimit) {
+		this.loadLimit = loadLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= loadLimit) {
+			deposit(amount);
+		}
+	}
+	
+}
