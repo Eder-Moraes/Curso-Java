3d8ca4551a3718762b012b076e8be16b7db8d662
Eder-Moraes <moraeseder880@gmail.com>
2024-12-06 10:06:57 -0300

    Classes e Metodos Final

---
+++ Herança e Polimorfismo/HerencaPolimorfismo/src/aula130_heranca/entities/SavingsAccountPlus.java
@@ -0,0 +1,10 @@
+package aula130_heranca.entities;
+
+public class SavingsAccountPlus extends SavingsAccount{
+	
+//	@Override
+//	public void withdraw(double amount) {
+//		balance -= amount + 2.0;
+//	}
+	
+}
